6dbb72abe1a5291a001778a46aab36f7664be3de
Peter <petermagar4@gmail.com>
2024-02-14 16:08:24 -0500

    Initial Commit

---
+++ Icebreaker/IcebreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcebreakerApp.swift
+//  Icebreaker
+//
+//  Created by Peter on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct IcebreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
